638f1dad2a9b24c95be9e7c89793da0ac6be921d
Miguel Díaz <mdiazl@vectoritcgroup.com>
2021-06-16 08:28:04 +0200

    Prácticas completas

---
+++ src/components/CustomForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="custom-form">
+        <form>
+            <div>
+                {{fraseEscrita}}
+            </div>
+            <input
+                v-model="fraseEscrita"
+                v-bind:disabled="!iniciado"
+            />
+            <button
+                v-on:click.prevent="empezar"
+            >
+                Empezar
+            </button>  
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomForm',
+    props: {
+        fraseInicial: String,
+    },
+    data: function(){
+        return {
+            datos: {
+            fraseEscrita: 'Bu',
+            },
+            iniciado: false,
+        }
+    },
+    methods: {
+        empezar: function(){
+            this.iniciado = true;
+        },
+    },
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Juego de escribir</h1>
+        <h2>{{fraseInicial}}</h2>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomHeader',
+    props: {
+        fraseInicial: String,
+    }
+}
+</script>
+<style>
+
+</style>
